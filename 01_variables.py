5bff337027ee93c57257d7999549c0980b40d3c3
David <davidbmjt@hotmail.com>
2023-12-04 10:56:01 +0100

    Primeros cambios para Git

---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+# Crear una variable en Python
+nombre = "David" # tipo String
+numero1 = 10 
+numero2 = 10.5
+
+boleano1 = True
+boleano2 = False
+
+print(nombre)
+print(type(numero1))
---
+++ main.py
@@ -0,0 +1,27 @@
+print("Hola")
+# mi primera variable
+saludo = "Hola" #mi primera variable
+
+numero1 = 1
+print(numero1)
+print(type(numero1))
+
+numero1 = "uno"
+print(numero1)
+
+'''   
+fdfsd
+'''
+
+print(type(numero1))
+
+verdadero = False
+print(type(verdadero))
+
+
+num1, num2, num3 = 1, 2, 3
+print(num1, num2, num3)
+# import this
+
+total = num1 + num2
+print(total)
